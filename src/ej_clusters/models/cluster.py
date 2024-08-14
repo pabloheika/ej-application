35cde2195ba1a626a24dfcb2f296efa52993587c
augustocrmg <augusto.duraes33@gmail.com>
2024-08-14 01:44:19 -0300

    Correção atribuição dataframe
    Co-authored-by: Eduardo Rodrigues eduardo.rfarias@outlook.com
    Co-authored-by: ggomesbr gabriel.luiz77888@gmail.com

--- src/ej_clusters/models/cluster.py
+++ src/ej_clusters/models/cluster.py
@@ -161,7 +161,11 @@ class Cluster(TimeStampedModel):
                     how="inner",
                     suffixes=["", "_"],
                 )
-
+            
+                df.sort_values(by=["content", "created"], inplace=True)
+                
+            else:
+                df = cluster_df.copy()
                 df.sort_values(by=["content", "created"], inplace=True)
 
         return df
