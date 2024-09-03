fcc7d03d89d6c7652846711b8b5dc81ac56fb4d6
Shayane <shayaneliebe@gmail.com>
2024-09-02 21:55:02 -0300

    Add results style and move insights

--- src/ej_conversations/views.py
+++ src/ej_conversations/views.py
@@ -541,6 +541,9 @@ class ConversationParticipantResults(DetailView):
         least_convergent_comments = df.sort_values("convergence", ascending=True)[:3]
         most_agreed_comments = df.sort_values("agree", ascending=False)[:3]
         most_disagreed_comments = df.sort_values("disagree", ascending=False)[:3]
+        print(least_convergent_comments)
+        print(most_agreed_comments)
+        print(most_disagreed_comments)
 
         return {"has_minimum_comments":conversation.has_minimum_comments(), 
                 "has_minimum_participant_votes": conversation.has_minimum_participant_votes(user),
